de2348324667ee36ddebb61503a6260303a8b253
Pedroth <pedrotiago92@gmail.com>
2024-08-12 15:15:22 +0100

    Update updateJavaDocker.sh

--- docker/updateJavaDocker.sh
+++ docker/updateJavaDocker.sh
@@ -1,2 +1,2 @@
-docker build -t pedroth/java-apps:latest -t pedroth/java-apps:v1.0.2 .
+docker build -t pedroth/java-apps:latest -t pedroth/java-apps:v1.0.2 . # update version
 docker push pedroth/java-apps:latest
