fceeb61ad4fa6fece406088fae82e71681243757
pedroth <pedrotiago92@gmail.com>
2026-07-15 18:18:51 +0100

    update version of docker

--- docker/updateJavaDocker.sh
+++ docker/updateJavaDocker.sh
@@ -2,7 +2,7 @@
 set -eu
 
 IMAGE=pedroth/java-apps
-VERSION=${VERSION:-v1.0.9}
+VERSION=${VERSION:-v1.0.10}
 
 # Run this script from the docker folder.
 docker build --build-arg CACHEBUST="$(date +%s)" -t "$IMAGE:latest" -t "$IMAGE:$VERSION" .
